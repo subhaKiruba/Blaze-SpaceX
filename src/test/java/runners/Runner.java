84dbcf2a09dc7b7feeaf9a17f3f16bc284df166f
subhaKiruba <subhashinir0703@gmail.com>
2021-03-15 15:31:43 +0530

    Second commit

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,7 +6,7 @@ import cucumber.api.*;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = "stepDefinition", tags = {
+@CucumberOptions(features = "src/test/java/features", glue = "stepDefinition", tags = {
 		"@TravelAgency" }, monochrome = true, plugin = { "pretty",
 				"com.blazeDemo.extentReports.ExtentCucumberFormatter:", "junit:target/cucumber-reports/cucumber.xml" })
 
